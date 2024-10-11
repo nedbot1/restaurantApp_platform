19a33e68308039d94ed30c677e54ec29ff544117
tobden <tobdheng@gmail.com>
2024-10-11 15:32:25 +0600

    updates routes for fetching menu with category

--- lib/restaurantApp_platform/menus.ex
+++ lib/restaurantApp_platform/menus.ex
@@ -1,119 +1,45 @@
 defmodule RestaurantAppPlatform.Menus do
-  @moduledoc """
-  The Menus context.
-  """
-
   import Ecto.Query, warn: false
   alias RestaurantAppPlatform.Repo
-
   alias RestaurantAppPlatform.Menus.Menu
 
   @doc """
-  Returns the list of menus.
-
-  ## Examples
-
-      iex> list_menus()
-      [%Menu{}, ...]
-
+  Get all menus for a restaurant grouped by category.
   """
-  def list_menus do
-    Repo.all(Menu)
+  def get_menus_by_restaurant_id(restaurant_id) do
+    query = from(m in Menu,
+      where: m.restaurant_id == ^restaurant_id,
+      preload: [:category]
+    )
+
+    menus = Repo.all(query)
+    IO.inspect(menus, label: "Fetched Menus for Restaurant")  # Debugging fetched menus
+
+    # Group the menus by category name
+    menus
+    |> Enum.group_by(fn menu -> menu.category && menu.category.name || "No Category" end)
   end
 
   @doc """
-  Gets a single menu.
-
-  Raises `Ecto.NoResultsError` if the Menu does not exist.
-
-  ## Examples
-
-      iex> get_menu!(123)
-      %Menu{}
-
-      iex> get_menu!(456)
-      ** (Ecto.NoResultsError)
-
+  Create a batch of menus.
   """
-  def get_menu!(id), do: Repo.get!(Menu, id)
+  # def create_menus(menus_params) do
+  #   Enum.map(menus_params, fn params ->
+  #     create_menu(params)
+  #   end)
+  # end
 
- def get_menus_by_restaurant_id(restaurant_id) do
-  query = from(m in Menu, where: m.restaurant_id == ^restaurant_id)
-     Repo.all(query)
-     |> Repo.preload(:category)
-  end
-
+  def create_menus(menus_params) do
+  Enum.map(menus_params, &create_menu/1)
+end
 
 
   @doc """
-  Creates a menu.
-
-  ## Examples
-
-      iex> create_menu(%{field: value})
-      {:ok, %Menu{}}
-
-      iex> create_menu(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Create a menu.
   """
   def create_menu(attrs \\ %{}) do
     %Menu{}
     |> Menu.changeset(attrs)
     |> Repo.insert()
-  end
-
-
-  def create_menus(menus_params) do
-    Enum.map(menus_params, fn params ->
-      create_menu(params)
-    end)
-  end
-
-  @doc """
-  Updates a menu.
-
-  ## Examples
-
-      iex> update_menu(menu, %{field: new_value})
-      {:ok, %Menu{}}
-
-      iex> update_menu(menu, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_menu(%Menu{} = menu, attrs) do
-    menu
-    |> Menu.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a menu.
-
-  ## Examples
-
-      iex> delete_menu(menu)
-      {:ok, %Menu{}}
-
-      iex> delete_menu(menu)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_menu(%Menu{} = menu) do
-    Repo.delete(menu)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking menu changes.
-
-  ## Examples
-
-      iex> change_menu(menu)
-      %Ecto.Changeset{data: %Menu{}}
-
-  """
-  def change_menu(%Menu{} = menu, attrs \\ %{}) do
-    Menu.changeset(menu, attrs)
   end
 end
--- lib/restaurantApp_platform_web/controllers/menu_json.ex
+++ lib/restaurantApp_platform_web/controllers/menu_json.ex
@@ -2,17 +2,30 @@ defmodule RestaurantAppPlatformWeb.MenuJSON do
   alias RestaurantAppPlatform.Menus.Menu
 
   @doc """
-  Renders a list of menus.
+  Renders a list of menus grouped by category.
   """
-  def index(%{menus: menus}) do
-    %{data: for(menu <- menus, do: data(menu))}
-  end
+
+def index(%{menus_by_category: menus_by_category}) do
+  # IO.inspect(menus_by_category, label: "Menus by Category in JSON")
+
+  %{
+    data: for {category_name, menus} <- menus_by_category do
+      %{category_name: category_name, menus: render_menus(menus)}
+    end
+  }
+end
+
 
   @doc """
   Renders a single menu.
   """
   def show(%{menu: menu}) do
+    # IO.inspect(menu, label: "Menu in Show JSON")
     %{data: data(menu)}
+  end
+
+  defp render_menus(menus) do
+    for menu <- menus, do: data(menu)
   end
 
   defp data(%Menu{} = menu) do
@@ -24,10 +37,7 @@ defmodule RestaurantAppPlatformWeb.MenuJSON do
       dish_photo_link: menu.dish_photo_link,
       restaurant_id: menu.restaurant_id,
       category_id: menu.category_id,
-      category_name: get_category_name(menu)  # Use a helper function to safely access category name
+      category_name: menu.category && menu.category.name || "No Category"
     }
   end
-
-  defp get_category_name(%Menu{category: nil}), do: "No Category"
-  defp get_category_name(%Menu{category: category}), do: category.name
 end
